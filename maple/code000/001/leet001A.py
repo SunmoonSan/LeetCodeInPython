f06d53d1f2fceccab8c78b17d7556ae030612697
San <18814184179@163.com>
2018-09-29 22:29:29 +0800

    1. 两数之和

--- maple/code000/001/leet001A.py
+++ maple/code000/001/leet001A.py
@@ -7,21 +7,21 @@
 
 
 class Solution:
-	def twoSum(self, nums, target):
-		"""
-		:type nums: List[int]
-		:type target: int
-		:rtype: List[int]
-		"""
-		for i in range(len(nums)):  # i是第一层索引
-			for j in range(i + 1, len(nums)):  # j是第二层索引
-				if nums[i] + nums[j] == target:  # 索引i位置的数和索引j位置的数和为target
-					return [i, j]  # 返回结果为list类型
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):  # i是第一层索引
+            for j in range(i + 1, len(nums)):  # j是第二层索引
+                if nums[i] + nums[j] == target:  # 索引i位置的数和索引j位置的数和为target
+                    return [i, j]  # 返回结果为list类型
 
 
 if __name__ == '__main__':
-	nums = [2, 7, 11, 15]
-	target = 9
-	solution = Solution()
-	p = solution.twoSum(nums, target)
-	print(p)
+    nums = [2, 7, 11, 15]
+    target = 9
+    solution = Solution()
+    p = solution.twoSum(nums, target)
+    print(p)
